b3a47ad9d7a44d3e1f782f1183d4d091e84bcf0c
trquockiet24 <trquockiet24@gmail.com>
2024-12-12 13:23:53 +0700

    backend setup

---
+++ backend/server.js
@@ -0,0 +1,32 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+import connectDB from "./src/lib/db.js";
+
+//routes
+import authRoute from "./src/routes/auth.route.js";
+import messageRoute from "./src/routes/auth.route.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
+app.use("/api/auth", authRoute);
+app.use("/api/messages", messageRoute);
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+  connectDB();
+});
